ac34727362b2c8258aa8e0d010cb6f4b5a3e6091
Danixou <dbellotorres@gmail.com>
2023-08-27 15:37:20 -0600

    Conexion: Base-Pagina
    -Se conecto la información de la base de datos con una página web

---
+++ AWSDBConnection/AWSDBConnection/AWSDBConnection/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AWSDBConnection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AWSDBConnection.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ArticulosEntity> Articulo { get; set; }
+    }
+}
---
+++ AWSDBConnection/AWSDBConnection/AWSDBConnection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AWSDBConnection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
